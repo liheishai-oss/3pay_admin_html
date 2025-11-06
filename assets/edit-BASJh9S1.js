9e6433562d516a728f54a37fe7e2a58ab365108c
liheishai-oss <liheishai@gmail.com>
2025-11-06 14:37:07 +0800

    修正接口地址为 3pay.api.baiyi-pay.com，并更新系统名称为百亿三方系统

--- assets/edit-BASJh9S1.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-Q4wbox1m.js";import"./index-_X_07Eu8.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 */export{o as default};
-//# sourceMappingURL=edit-BASJh9S1.js.map
---
+++ assets/edit-BTKxNgI6.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-BTN7Atmo.js";import"./index-Bd9DWvNd.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 */export{o as default};
+//# sourceMappingURL=edit-BTKxNgI6.js.map
