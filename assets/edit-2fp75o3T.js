68a18a905b3b73007754115ace025ec0f7a0fe18
liheishai-oss <liheishai@gmail.com>
2025-11-07 12:07:59 +0800

    feat: 更新商户管理功能
    - 移除商户联系邮箱、回调通知地址、同步返回地址字段
    - 新增商户账号自动创建功能，默认密码123456，首次登录需修改
    - 移除登录页面的保持会话选项
    - 修复代理商菜单显示问题
    - 新增对接资料功能，包含熔断机制说明
    - 修复重置密钥后数据不更新问题
    - 优化商户列表数据格式，修复数据不显示问题

---
+++ assets/edit-2fp75o3T.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-BNBFrPmu.js";import"./index-CFTblcIy.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 */export{o as default};
+//# sourceMappingURL=edit-2fp75o3T.js.map
--- assets/edit-BTKxNgI6.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-BTN7Atmo.js";import"./index-Bd9DWvNd.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 */export{o as default};
-//# sourceMappingURL=edit-BTKxNgI6.js.map
