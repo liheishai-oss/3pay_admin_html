b8ed11369984df21913a05d2f46e6b2ec559d067
liheishai-oss <liheishai@gmail.com>
2025-11-11 11:33:10 +0800

    更新前端打包文件
    - 修复订单创建时的商品描述参数处理
    - subject参数改为可选，未传递时使用默认值
    - body参数未传递时使用subject的值

---
+++ assets/edit-Bfu7qvG2.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-B5WlQv6q.js";import"./index-eDxl0EL6.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 */export{o as default};
+//# sourceMappingURL=edit-Bfu7qvG2.js.map
