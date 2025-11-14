0dda8ea20a60011a46595d169c1ca664bf0e44d7
liheishai-oss <liheishai@gmail.com>
2025-11-14 19:49:01 +0800

    更新前端打包文件
    - 从 3pay_admin/dist 复制最新的打包文件
    - 更新产品管理页面：移除代理商显示，只显示当前代理商的产品

--- assets/edit-B4FmD11e.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-B-g0qjzT.js";import"./index-ByJsN6Y7.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 */export{o as default};
-//# sourceMappingURL=edit-B4FmD11e.js.map
---
+++ assets/edit-DZOA4TlQ.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-DN-ds2dU.js";import"./index-BtkdBdlp.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 */export{o as default};
+//# sourceMappingURL=edit-DZOA4TlQ.js.map
